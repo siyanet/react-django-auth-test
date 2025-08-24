3da886add92232f12a3268446909fa322939fd23
siyanet <siyangetahunw@gmail.com>
2025-08-24 23:59:52 +0300

    front and back setup done

---
+++ YayaFrontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello React + Tailwind + TypeScript!
+      </h1>
+    </div>
+  );
+}
+export default App;
